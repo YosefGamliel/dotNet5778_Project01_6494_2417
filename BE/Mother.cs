0239733fcd5125d9c3eb3951d9f0ff8eed51c949
shlomi batito <shlomi.batito616@gmail.com>
2017-12-16 23:16:15 +0200

    less in mother class section z section HET section YOD

---
+++ BE/Mother.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BE
+{
+    class Mother :Person
+    {
+      
+        private string areaBabysitter;//where the mother search baby sitter
+        //seif z
+        //seif h
+        //seif yud
+        private string special;//Remarks or Requirements
+    }
+}
---
+++ BE/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BE
+{
+    class Person
+    {
+        private int id;
+        private string LastName;
+        private string FirstName;
+        private int phone;
+        private string address;
+    }
+}
