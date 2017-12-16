320e78bb99d03469cd498920365353a1663be3f1
yosef gamliel <yosefgam@walla.com>
2017-12-16 23:20:08 +0200

    nanny

---
+++ BE/Nanny.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BE
+{
+    class Nanny
+    {
+        private int ID;
+        private string lastName;
+        private string firstName;
+        private DateTime birthday;
+        private int phoneNumber;
+        string address;
+
+    }
+}
