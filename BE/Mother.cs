310f29e3251f76490d9c610f00ad4d836c955ac4
shlomi batito <shlomi.batito616@gmail.com>
2017-12-17 00:12:56 +0200

    good night;

--- BE/Mother.cs
+++ BE/Mother.cs
@@ -4,13 +4,22 @@ using System.Text;
 
 namespace BE
 {
-    class Mother :Person
+    class Mother : Person
     {
-     
+
         private string areaBabysitter;//where the mother search baby sitter
-        //seif z
-        //seif h
-        //seif yud
-        private string Note;//Remarks or Requirements
-    }
+        private bool[] needbabysitter = new bool[7];   //need the baby sitter
+        /*
+        DayOfWeek[][] WorkHour = new DayOfWeek[6][]
+        {
+        new Hours[2]{ , },
+        new Hours[2]{, },
+        new Hours[2]{, },
+        new Hours[2]{, },
+        new Hours[2]{, },
+        new Hours[2]{, };
+
+    }*/
+    private string Note;//Remarks or Requirements
+}
 }
--- BE/Person.cs
+++ BE/Person.cs
@@ -12,4 +12,5 @@ namespace BE
         private int phone;
         private string address;
     }
+
 }
