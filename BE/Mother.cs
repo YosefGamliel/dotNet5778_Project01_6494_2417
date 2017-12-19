b2902474f91a06e8a529053f250254275bee2466
shlomi batito <shlomi.batito616@gmail.com>
2017-12-19 09:19:01 +0200

    סידרתי את הסטבדיאוףוויק

--- BE/Mother.cs
+++ BE/Mother.cs
@@ -12,17 +12,20 @@ namespace BE
         private DaysOfWeek[][] workHour = new DaysOfWeek[5][];
         private string note;//Remarks or Requirements
         //properties:
-        public string AreaBabysitter { get { return areaBabysitter;} set { areaBabysitter = value; } }
+        public string AreaBabysitter { get { return areaBabysitter; } set { areaBabysitter = value; } }
         public bool[] NeedBabysitter { get { return needBabysitter; } set { needBabysitter = value; } }
         public string Note { get { return note; } set { note = value; } }
         public DaysOfWeek[][] WorkHour
         {
             get { return workHour; }
-            /*set
+            set
             {
-                for (int i=0;i<5;++i)
-                    
-            }*/
+                for (int i = 0; i < 5; ++i)
+                {
+                    workHour[i] = value[i];//מאתחל את השעות שבכל יום
+                }
+
+            }
         }
         //functions:
     }
