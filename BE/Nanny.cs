9b02a6ab71bed75a17362d49bcf01c85908e5745
yosef gamliel <yosefgam@walla.com>
2017-12-20 22:02:09 +0200

    ניח

--- BE/Nanny.cs
+++ BE/Nanny.cs
@@ -51,5 +51,58 @@ namespace BE
         public bool VacationDaysITE { get { return vacationDaysITE; } set { vacationDaysITE = value; } }
         public string Recommendations { get { return recommendations; } set { recommendations = value; } }
         #endregion
+        //functions:
+        #region
+        public Nanny(int ID, string LN, string FN,int PN,string addr, DateTime birth,bool elev,int FB,int EY,int MK,
+            int MinA,int MaxA,bool HR,float HS,float MS,bool[] WD,int[,] WH,bool VD,string recomm)
+        {
+            id = ID;
+            lastName = LN;
+            firstName = FN;
+            phoneNumber = PN;
+            address = addr;
+            birthday = birth;
+            elevator = elev;
+            floorBuilding = FB;
+            experienceYears = EY;
+            maxKids = MK;
+            minAge = MinA;
+            maxAge = MaxA;
+            hourlyRate = HR;
+            hourSalary = HS;
+            monthSalary = MS;
+            workDays = WD;
+            workHours = new int[6, 2];
+            workHours = WH;
+            vacationDaysITE = VD;
+            recommendations = recomm;
+        }
+        public override string ToString()
+        {
+            string str1 = null, str2 = null,str3=null, WH = "The hours she works each day:", WORK = "Days when she works: ";
+            if (elevator) str1 = "Yes";
+            else str1 = "No";
+            if (hourlyRate) str2 = "Yes";
+            else str2 = "No";
+            for (int i = 0; i < 6; ++i)
+            {
+                WORK += ((DaysOfWeek)i).ToString();
+                if (workDays[i])
+                    WORK += ": Yes ";
+                else
+                    WORK += ": No ";
+                WH += ("\n" + ((DaysOfWeek)i).ToString() + "- Beginning time: " + (workHours[i, 0] / 100) + ":"
+                    + (workHours[i, 0] % 100) + "End time: " + (workHours[i, 1] / 100) + ":" + (workHours[i, 1] % 100));
+                if (vacationDaysITE)
+                    str3 = "gets her vacation days according to the Ministry of Industry, Trade and Employment";
+                else str3 = "gets her vacation days according to the Ministry of Education";
+            }
+            return "Id: " + id + "\nFirst name: " + firstName + "\nLast name: " + lastName + "\nPhone number: " + phoneNumber
+                + "\nAddress: " + address + "\nBirthday: " + birthday + "\nElevator in the building: " + str1 + "\nFloor in the building: "
+                + floorBuilding + "\nExperience years: " + experienceYears + "\nMaximum of kids: " + maxKids + "\nAges: from age " + minAge +
+                " months to age " + maxAge + " months\nHourly rate: " + str2 + "\nMonth salary: " + monthSalary + "\n" + WORK + "\n" + WH + "\n"
+                + str3 + "\nRecommendations" + recommendations;
+        }
+        #endregion
     }
 }
