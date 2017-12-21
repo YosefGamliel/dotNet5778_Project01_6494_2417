775c729e01e8e03fc5b491697d2cb944929cb2a8
shlomi batito <shlomi.batito616@gmail.com>
2017-12-21 18:46:29 +0200

    Idal

---
+++ DAL/Idal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    interface Idal
+    {
+        void add(object obj);
+        void remove(object obj);
+        void update(object obj);
+    }
+}
