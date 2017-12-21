267aa1657ec89d49d5e1487eff38c2fcba88a23c
shlomi batito <shlomi.batito616@gmail.com>
2017-12-21 21:06:03 +0200

    chikd

--- BE/child.cs
+++ BE/child.cs
@@ -31,6 +31,7 @@ namespace BE
             id = ID;
             try
             {
+                if (!CheckIDNo(ID)) throw;
 
             }
             firstName = FN;
@@ -48,6 +49,28 @@ namespace BE
                 str2 = "\nInformation about the special needs: " + infoSpecialNeeds;
             return "Id: " + id + "\nFirst name: " + firstName + "\nMother id: " + motherId +
                 "\nBirthday: " + birthday + "\nSpecial needs: " + str1 + str2;
+        }
+        static bool CheckIDNo(String strID)
+        {
+            int[] id_12_digits = { 1, 2, 1, 2, 1, 2, 1, 2, 1 };
+            int count = 0;
+
+            if (strID == null)
+                return false;
+
+            strID = strID.PadLeft(9, '0');
+
+            for (int i = 0; i < 9; i++)
+            {
+                int num = Int32.Parse(strID.Substring(i, 1)) * id_12_digits[i];
+
+                if (num > 9)
+                    num = (num / 10) + (num % 10);
+
+                count += num;
+            }
+
+            return (count % 10 == 0);
         }
         #endregion
     }
