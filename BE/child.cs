b3f2763fdb38af2ad161d807021d65ea0c020210
yosef gamliel <yosefgam@walla.com>
2017-12-22 11:06:21 +0200

    mbjkd

--- BE/child.cs
+++ BE/child.cs
@@ -54,28 +54,7 @@ namespace BE
             return "Id: " + id + "\nFirst name: " + firstName + "\nMother id: " + motherId +
                 "\nBirthday: " + birthday + "\nSpecial needs: " + str1 + str2;
         }
-        public static bool CheckIDNo(String strID)
-        {
-            int[] id_12_digits = { 1, 2, 1, 2, 1, 2, 1, 2, 1 };
-            int count = 0; 
-
-            if (strID == null)
-                return false;
-
-            strID = strID.PadLeft(9, '0');
-
-            for (int i = 0; i < 9; i++)
-            {
-                int num = Int32.Parse(strID.Substring(i, 1)) * id_12_digits[i];
-
-                if (num > 9)
-                    num = (num / 10) + (num % 10);
-
-                count += num;
-            }
-
-            return (count % 10 == 0);
-        }
+        
         #endregion
     }
 }
