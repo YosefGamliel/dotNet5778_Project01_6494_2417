a040c8edbaec256bfc34a7aa74d6abd9abcc420a
shlomi batito <shlomi.batito616@gmail.com>
2017-12-24 19:34:25 +0200

    אני סיימתי את הפונקציות של האמא רשמתי הערות עם משהו לא מובן

--- DAL/Dal_imp.cs
+++ DAL/Dal_imp.cs
@@ -9,25 +9,78 @@ namespace DAL
 {
     class Dal_imp : Idal
     {
-        public void add(Nanny nanny)
+        #region
+        public void addNanny(Nanny nanny)
         {
             try
             {
-                foreach (Nanny item in getList())
+                foreach (Nanny item in getNannyList())
                 {
                     if (item.Id == nanny.Id)
                         throw new Exception();
                 }
-                getList().Add(nanny);
+                getNannyList().Add(nanny);
             }
         }
-        public List<Nanny> getList()
+        public List<Nanny> getNannyList()
         {
             return DataSource.NannyList;
         }
-        public List<Contract> getList()
+        #endregion
+/// <summary>
+/// Mother function
+/// </summary>
+/// <param name="mother"></param>
+        #region
+        public void addMother(Mother mother)
         {
+            try
+            {
+                foreach (Mother item in getMotherList())
+                {
+                    if (item.Id == mother.Id)
+                        throw new Exception();
+                }
+                getMotherList().Add(mother);
+            }
+        }
+        public List<Mother> getMotherList()
+        {
+            return DataSource.MotherList;
+        }
+        public void removeMother(Mother mother)
+        {
+            bool flag = true;
+            try
+            {
+                foreach (Mother item in getMotherList())
+                {
+                    if (item.Id == mother.Id)//if find id to delete
+                        flag =false;
+                }
+                if (flag)//id to delete not found throw Exception
+                    throw new Exception();
+                getMotherList().Remove(mother);
+            }
+        }
+        void updateMother(Mother mother)
+        {
+            bool flag = true;
+            try
+            {
+                foreach (Mother item in getMotherList())
+                {
+                    if (item.Id == mother.Id)//if find id to delete
+                        flag = false;
+                }
+                if (flag)//id to delete not found throw Exception
+                    throw new Exception();
+                getMotherList().Remove(mother);//delete the old mother
+                getMotherList().Add(mother);//insert the update mother
+
+            }
 
         }
+        #endregion
     }
 }
--- DAL/Dal_imp2.cs
+++ DAL/Dal_imp2.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using BE;
+using DS;
 namespace DAL
 {
-    class Dal_imp2
+    class Dal_imp2 : Idal
     {
+       
     }
 }
