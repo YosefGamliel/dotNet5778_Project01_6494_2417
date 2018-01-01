a58848c826858d3c304b544831ecfad5fc2454bf
yogamlie <yogamlie@L320-52-229>
2018-01-01 16:07:01 +0200

    dsadas

--- BL/BL_imp2.cs
+++ BL/BL_imp2.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using BE;
+using DAL;
 namespace BL
 {
-
-    class BL_imp2
+    class BL_imp2 : IBL
     {
+        Dal_imp dal = new Dal_imp();
+        public void addContract(Contract contract)
+        {
+            try
+            {
+                
+                foreach (Child item in getChildList())
+                {
+                    if ()
+                }
+                dal.addContract(contract);
+
+            }
+        }
     }
 }
