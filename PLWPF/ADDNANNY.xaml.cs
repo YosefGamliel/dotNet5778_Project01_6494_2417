fa80d3746117b5ec2380fac4af8a23f946cea847
shlomi batito <shlomi.batito616@gmail.com>
2018-01-15 21:20:08 +0200

    555

---
+++ PLWPF/ADDNANNY.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PLWPF
+{
+    /// <summary>
+    /// Interaction logic for ADDNANNY.xaml
+    /// </summary>
+    public partial class ADDNANNY : Window
+    {
+        public ADDNANNY()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource nannyViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("nannyViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // nannyViewSource.Source = [generic data source]
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource nannyViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("nannyViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // nannyViewSource.Source = [generic data source]
+        }
+    }
+}
